0603aba2ddba737753e65f599f6f447fcdcb0d73
Kendal Sosa <kendal.alfonso37@gmail.com>
2018-06-03 17:40:18 -0600

    Cambiado script de Punto Medio para que funcione con Interfaz Grafica. Progreso: 40% - 100%

--- scripts/puntomedio.m
+++ scripts/puntomedio.m
@@ -6,29 +6,34 @@
 ## margen de error del metodo
 
 function puntomedio
-  printf("Metodo de Integracion numerica a traves del metodo del Punto Medio\n");
+  #printf("\n");
   #Entrada de datos
-  g = input("Ingrese la Funcion: ", 'S');
-  f=inline(g); #Convierte el string a una funcion para evaluar
-  a=input("Ingrese el extremo inferior de la Integral: ");
-  b=input("Ingrese el extremo superior de la Integral: ");
-  n=input("Ingrese la cantidad de puntos medios a utilizar:");
+  g = inputdlg("Ingrese la Funcion: ", "Metodo de Integracion por Punto Medio");
+  f=inline(g{1,1}); #Convierte el string a una funcion para evaluar
+  a=inputdlg("Ingrese el extremo inferior de la Integral: ");
+  b=inputdlg("Ingrese el extremo superior de la Integral: ");
+  n=inputdlg("Ingrese la cantidad de puntos medios a utilizar:");
+  
+  # Conversiones de datos para la integracion
+  aa = str2num(a{1,1});
+  bb = str2num(b{1,1});
+  nn = str2num(n{1,1});
   
   #Vectores para plotear mas adelante
-  x = [a:0.05:b];
-  rx = linspace(a,b,n);
+  x = [aa:0.05:bb];
+  rx = linspace(aa,bb,nn);
   
   #Obteniendo integral numerica mas aproximada:
-  q = quad(f,a,b);
+  q = quad(f,aa,bb);
   
   #Definiendo la h inicial
-  h=(b-a)/n;
+  h=(bb-aa)/nn;
   S=0;
   # Aplicacion de la regla del punto medio:
   tic;
-  for i = 1:n
-    S = S + (h)*(f((a+(a+h))/2));
-    a=a+h;
+  for i = 1:nn
+    S = S + (h)*(f((aa+(aa+h))/2));
+    aa=aa+h;
   endfor
   t1=toc;
   #Fin del metodo del punto medio
@@ -37,13 +42,14 @@ function puntomedio
   err = S.-q;
   
   #salida de datos
-  printf("El valor Aproximado de la integral es: %f \n", S);
-  printf("Tiempo transcurrido: %f Segundos\n", t1);
-  printf("La cota de error del metodo es de: %f \n", abs(err));
+  p1 = strcat("El valor Aproximado de la integral es: ", num2str(S), "\nTiempo transcurrido: ", num2str(t1), " Segundos\nLa cota de error del metodo es de: ", num2str(abs(err)), "");
+  #printf("", );
+  #printf(" %f \n", );
+  msgbox(p1, "Resultados de la Integral", "help");
   
   #Ploteo de las Funciones:
   plot(x,f(x),'r')
   hold on
-  plot(rx, f(rx), 'g')
+  plot(rx, f(rx), 'm')
   
 endfunction
